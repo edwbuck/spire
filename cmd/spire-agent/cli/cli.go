5b86ada63144069fdc6e42c5b782f3aeb9af987e
Edwin Buck <edwbuck@gmail.com>
2024-04-05 07:56:06 -0500

    Remove dynamic CLI presentation, at request of Andrew.
    Now, if the agent admin socket is not set, the desired
    approach to handle it is to have the cli command still
    present itself as available, but the command should fail.
    
    Signed-off-by: Edwin Buck <edwbuck@gmail.com>

--- cmd/spire-agent/cli/cli.go
+++ cmd/spire-agent/cli/cli.go
@@ -2,13 +2,8 @@ package cli
 
 import (
 	"context"
-	"flag"
-	"io"
 	stdlog "log"
-	"os"
-	"strings"
 
-	"github.com/hashicorp/hcl"
 	"github.com/mitchellh/cli"
 	"github.com/spiffe/spire/cmd/spire-agent/cli/api"
 	"github.com/spiffe/spire/cmd/spire-agent/cli/healthcheck"
@@ -19,71 +14,12 @@ import (
 	"github.com/spiffe/spire/pkg/common/version"
 )
 
-const (
-	defaultConfigPath = "conf/agent/agent.conf"
-)
-
-type Config struct {
-	Agent *agentConfig `hcl:"agent"`
-}
-
-type agentConfig struct {
-	AdminSocketPath string `hcl:"admin_socket_path"`
-}
-
 type CLI struct {
 	LogOptions         []log.Option
 	AllowUnknownConfig bool
 }
 
-func ConfigFilePath(args []string) string {
-	for len(args) > 0 && !strings.HasSuffix(args[0], "-config") {
-		args = args[1:]
-	}
-
-	var configPath string
-	flags := flag.NewFlagSet("", flag.ContinueOnError)
-	flags.StringVar(&configPath, "config", defaultConfigPath, "Path to a SPIRE config file")
-	flags.SetOutput(io.Discard)
-	flags.Parse(args)
-	return configPath
-}
-
-func ExpandEnv(args []string) bool {
-	for len(args) > 0 && !strings.HasSuffix(args[0], "-expandEnv") {
-		args = args[1:]
-	}
-
-	var expandEnv bool
-	flags := flag.NewFlagSet("", flag.ContinueOnError)
-	flags.BoolVar(&expandEnv, "expandEnv", false, "Expand environment variable in SPIRE config file")
-	flags.SetOutput(io.Discard)
-	flags.Parse(args)
-	return expandEnv
-}
-
-func AdminEnabled(path string, expandEnv bool) bool {
-	bytes, err := os.ReadFile(path)
-	if err != nil {
-		return false
-	}
-	config := string(bytes)
-	if expandEnv {
-		config = os.ExpandEnv(config)
-	}
-	c := Config{}
-	err = hcl.Decode(&c, config)
-	if err != nil {
-		return false
-	}
-	return c.Agent.AdminSocketPath != ""
-}
-
 func (cc *CLI) Run(ctx context.Context, args []string) int {
-	configPath := ConfigFilePath(args)
-	expandEnv := ExpandEnv(args)
-	adminEnabled := AdminEnabled(configPath, expandEnv)
-
 	c := cli.NewCLI("spire-agent", version.Version())
 	c.Args = args
 
@@ -112,17 +48,15 @@ func (cc *CLI) Run(ctx context.Context, args []string) int {
 		"validate": func() (cli.Command, error) {
 			return validate.NewValidateCommand(), nil
 		},
-	}
-	if adminEnabled {
-		c.Commands["logger get"] = func() (cli.Command, error) {
+		"logger get": func() (cli.Command, error) {
 			return logger.NewGetCommand(), nil
-		}
-		c.Commands["logger set"] = func() (cli.Command, error) {
+		},
+		"logger set": func() (cli.Command, error) {
 			return validate.NewValidateCommand(), nil
-		}
-		c.Commands["logger reset"] = func() (cli.Command, error) {
+		},
+		"logger reset": func() (cli.Command, error) {
 			return validate.NewValidateCommand(), nil
-		}
+		},
 	}
 
 	exitStatus, err := c.Run()
